6448383c5b7d8ece24c25d19eadeaf254a4b22aa
Aleksandr Petrosyan <a-p-petrosyan@yandex.ru>
2018-11-29 00:36:03 +0400

    Cleanup.

--- config.el
+++ config.el
@@ -1,4 +1,3 @@
-;; these functions are useful. activate them.
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 (put 'narrow-to-region 'disabled nil)
@@ -6,20 +5,27 @@
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
-;; (setq org-src-fontify-natively t)
-  (custom-set-variables
-   '(org-fontify-natively t)
-   )
+(custom-set-variables
+ '(org-fontify-natively t)
+ '(org-enforce-todo-dependencies t)
+ )
 
-(custom-set-variables '(global-linum-mode t))
+(custom-set-variables 
+'(global-linum-mode t)
+'(cursor-type 'bar)
+)
 
-(setq split-height-threshold nil)
-(setq split-width-threshold 0)
+(custom-set-variables 
+ '(split-height-threshold nil) 
+ '(split-width-threshold 0)
+ )
 
 (defun todo()
-(interactive)
-(find-file "/Users/app/Dropbox/org-notes/todo.org")
-)
+  (interactive)
+  (find-file "/Users/app/Dropbox/org-notes/todo.org")
+  )
+
+(server-start)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -39,7 +45,13 @@
   )
 
 (use-package org
-  :ensure org-plus-contrib)
+  :ensure org-plus-contrib
+  :bind 
+  ("s-<up>" . outline-previous-heading)
+  ("s-<down>" . outline-next-heading)
+  ("M-s-<up>" . org-babel-previous-src-block)
+  ("M-s-<down>" . org-babel-next-src-block)
+  )
 
 (use-package org-ref
   :ensure t)
@@ -49,18 +61,27 @@
   :mode ("/\\.emacs\\.d/snippets/" . snippet-mode)
   :init
   (custom-set-variables
-    '( yas-verbosity 3)
-    '(yas-global-mode 1)
+   '(yas-global-mode 1)
     )
+  :config
+  (custom-set-variables
+   '(yas-verbosity 3)
+   '(yas-wrap-around-region t)
+   '(yas-use-menu 'abbreviate)
+   )
   )
+  (define-key yas-minor-mode-map (kbd "<tab>") nil)
+  (define-key yas-minor-mode-map (kbd "TAB") nil)
+  (define-key yas-minor-mode-map (kbd "M-SPC") 'yas-expand)
 
 (use-package company
   :ensure t
   :commands company-mode
-  :init
-  (add-hook 'prog-mode-hook 'company-mode)
-  (add-hook 'LaTeX-mode-hook 'company-mode)
-  (add-hook 'org-mode-hook 'company-mode)
+  :hook ((prog-mode LaTeX-mode org-mode) . company-mode)
+  ;:init
+  ;(add-hook 'prog-mode-hook 'company-mode)
+  ;(add-hook 'LaTeX-mode-hook 'company-mode)
+  ;(add-hook 'org-mode-hook 'company-mode)
   :config
  (custom-set-variables
  '(company-idle-delay .5)
@@ -68,18 +89,16 @@
  '(company-echo-delay 1)
  '(company-minimum-prefix-length 2)
  '(company-selection-wrap-around t)
- '(company-transformers '(company-sort-by-occurrence company-sort-by-backend-importance ))
+ '(company-show-numbers t)
+ '(company-transformers
+   '(company-sort-by-occurrence company-sort-by-backend-importance )
+   ))
  )
-  :bind ("M-<Space>" . company-complete-common)
-  )
 
 (use-package company-quickhelp
   :ensure t
   :defer t
-  :hook global-company-mode
-  )
-
-(define-key global-map (kbd "C-.") 'company-files)
+  :hook global-company-mode )
 
 (use-package smartparens
   :ensure t
@@ -93,6 +112,8 @@
   ("M-]" . sp-forward-slurp-sexp)
   ("M-S-[" . sp-backward-barf-sexp)
   ("M-S-]" . sp-forward-barf-sexp)
+  ("C-c M-s s-u" . sp-unwrap-sexp)
+  ("C-c M-s s-r" . sp-rewrap-sexp)
   :config
   (require 'smartparens-config)
   ;; markdown
@@ -181,13 +202,13 @@
 
 (use-package latex-pretty-symbols
   :ensure t
+  ;:hook (LaTeX-mode . (prettify-symbols-mode LaTeX-math-mode turn-on-reftex))
   :init
   (progn 
-    (add-hook 'LaTeX-mode-hook 'prettify-symbols-mode)
-    (add-hook 'LaTex-mode-hook 'LaTeX-math-mode)
+    (add-hook 'LaTeX-mode-hook 'prettify-symbols-mode) 
+    (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
     (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
-    (setq reftex-plug-into-AUCTeX t)
-    (setq TeX-auto-save t)
+    (custom-set-variables '(reftex-plug-into-AUCTeX t) '(TeX-auto-save t))
     )
   )
 
@@ -196,31 +217,69 @@
  'python-mode-hook
  (lambda ()
    (mapc (lambda (pair) (push pair prettify-symbols-alist))
-         '(;; Syntax
-           ("def" .      #x2131)
-           ("not" .      #x2757)
-           ("in" .       #x2208)
-           ("not in" .   #x2209)
-           ("return" .   #x27fc)
-           ("yield" .    #x27fb)
-           ("for" .      #x2200)
-           ;; Base Types
-           ("int" .      #x2124)
-           ("float" .    #x211d)
-           ("str" .      #x1d54a)
-           ("True" .     #x1d54b)
-           ("False" .    #x1d53d)
-           ;; Mypy
-	   ("*"	.	 #x00d7)
-           ("Dict" .     #x1d507)
-           ("List" .     #x2112)
-           ("Tuple" .    #x2a02)
-           ("Set" .      #x2126)
-	   ("sum" . 	 #x2211)
-           ("Iterable" . #x1d50a)
-           ("Any" .      #x2754)
+	 '(;; Syntax
+	   ("def" .      #x2131)
+	   ("not" .      #x2757)
+	   ("in" .       #x2208)
+	   ("not in" .   #x2209)
+	   ("return" .   #x27fc)
+	   ("yield" .    #x27fb)
+	   ("for" .      #x2200)
+	   ;; Base Types
+	   ("int" .      #x2124)
+	   ("float" .    #x211d)
+	   ("str" .      #x1d54a)
+	   ("True" .     #x1d54b)
+	   ("False" .    #x1d53d)
+	   ;; Mypy
+	   ("*"	.  #x00d7)
+	   ("Dict" .     #x1d507)
+	   ("List" .     #x2112)
+	   ("Tuple" .    #x2a02)
+	   ("Set" .      #x2126)
+	   ("sum" . 	   #x2211)
+	   ("Iterable" . #x1d50a)
+	   ("Any" .      #x2754)
 	   ("lambda" .	 #x03bb)
-           ("Union" .    #x22c3)
+	   ("Union" .    #x22c3)
+	   )
+	 )
+   )
+ )
+
+(add-hook
+ 'c++-mode-hook
+ (lambda ()
+   (mapc (lambda (pair) (push pair prettify-symbols-alist))
+	 '(;; Syntax
+	   ("void" . #x2132)
+	   ("!" . #x2757)
+	   ("in" . #x2208)
+	   ("not in" . #x2209)
+	   ("return" . #x27fc)
+	   ;; ("cout" .     #x27fb)
+	   ("for" . #x2200)
+	   ;; Base Types
+	   ("bool" . #x2234)
+	   ("auto" . #x04D4)
+	   ("char" . #x2135)
+	   ("int" . #x2124)
+	   ("float" . #x211d)
+	   ("double" . #x211d)
+	   ("string" . #x1d54a)
+	   ("true" . #x1d54b)
+	   ("false" . #x1d53d)
+	   ;; Mypy
+	   ("*" . #x2217)
+	   ("unordered_map" . #x1d507)
+	   ("vector" . #x2112)
+	   ("tuple" . #x2a02)
+	   ("set" . #x2126)
+	   ("sum" . #x2211)
+	   ("Iterable" . #x1d50a)
+	   ("Any" . #x2754)
+	   ("lambda" . #x03bb)
+	   ("Union" . #x22c3)
 	   )
 	 )
    )
@@ -261,37 +320,37 @@
 
 (use-package org-bullets
   :ensure t
-  ;;:hook (org-mode . lambda () (org-bullets-mode 1))
-  :config (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))) 
+  :hook (org-mode . (lambda () (org-bullets-mode 1)))
   )
 
 (use-package rainbow-delimiters
   :ensure t 
   :commands rainbow-delimiters-mode
-  :init
-  (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
-  (add-hook 'LaTex-mode-hook #'rainbow-delimiters-mode)
-  (add-hook 'org-mode-hook #'rainbow-delimiters-mode)
+  :hook ((prog-mode LaTeX-mode org-mode) . rainbow-delimiters-mode)
+  ;; :init
+  ;; (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+  ;; (add-hook 'LaTex-mode-hook #'rainbow-delimiters-mode)
+  ;; (add-hook 'org-mode-hook #'rainbow-delimiters-mode)
   )
 
 (use-package flycheck
   :ensure t
-  :config (progn (add-hook 'after-init-hook #'global-flycheck-mode)
-		 (add-hook 'python-mode-hook (lambda ()
-					       (flycheck-select-checker 'python-pylint)))
-		 )
+  :hook (after-init . global-flycheck-mode)
+  :config   (add-hook 'python-mode-hook 
+  (lambda () (flycheck-select-checker 'python-pylint)))
   )
+
 
 (use-package flyspell
   :ensure t
   :defer t
+  :hook ((markdown-mode text-mode LaTeX-mode) . (lambda () (flyspell-mode 1)))
   :init
   (progn
     (setq-default ispell-program-name "aspell")
     (setq-default ispell-extra-args '("--sug-mode=fast"))
     (setq-default ispell-dictionary "english")
-    (add-hook 'markdown-mode-hook '(lambda () (flyspell-mode 1)))
-    (add-hook 'text-mode-hook '(lambda () (flyspell-mode 1))))
+    )
   :config
   )
 
@@ -311,6 +370,10 @@
   )
   )
 
+(use-package string-inflection
+  :ensure t
+  )
+
 (use-package cdlatex
   :ensure t
 )
@@ -319,22 +382,48 @@
   :defer t
   :ensure auctex
   :config
-  (setq TeX-auto-save t)
+  (custom-set-variables 
+   '(TeX-auto-save t)
+   )
   )
 
 (use-package ox-reveal
-  :ensure t)
-(setq org-reveal-root "http://cdn.jsdelivr.net/reveal.js/3.0.0/")
-;; (setq org-reveal-mathjax t)
+  :ensure t
+  :config 
+  (custom-set-variables 
+   '(org-reveal-root "http://cdn.jsdelivr.net/reveal.js/3.0.0/")
+   '(org-reveal-mathjax t)
+   )
+  )
 
-(global-set-key (kbd "C-M-;") 'comment-region-or-line )
-(global-set-key (kbd "C-M-:") 'uncomment-region-or-line )
-(global-set-key (kbd "s-c") 'kill-ring-save)
-(global-set-key (kbd "s-v") 'yank)
-(global-set-key (kbd "s-z") 'undo)
-(global-set-key (kbd "s-s") 'save-buffer)
+(global-set-key (kbd "C-M-;") 	'comment-line) 
+(global-set-key (kbd "C-M-:") 	'uncomment-region)
+(global-set-key (kbd "s-c") 		'kill-ring-save)
+(global-set-key (kbd "s-v") 		'yank)
+(global-set-key (kbd "s-z") 		'undo)
+(global-set-key (kbd "s-s") 		'save-buffer)
+(global-set-key (kbd "s-a") 		'mark-whole-buffer)
+(global-set-key (kbd "M-s-a") 	'outline-show-all)
+(global-set-key (kbd "s-<right>") 	'move-end-of-line)
+(global-set-key (kbd "s-<left>") 	'move-beginning-of-line)
+(global-set-key (kbd "s-<backspace>") 'kill-whole-line)
+(global-set-key (kbd "s-<delete>") 	'kill-line)
+(global-set-key (kbd "s-q") 		'save-buffers-kill-emacs)
+(global-set-key (kbd "s-w") 		'kill-buffer-and-window)
+(global-set-key (kbd "C-s-[") 	'shrink-window-horizontally)
+(global-set-key (kbd "C-s-]") 	'enlarge-window-horizontally)
+(global-set-key (kbd "s-, c") 'reconfigure)
+(global-set-key (kbd "s-, r") 'reload-config)
+(custom-set-variables
+ '(mac-command-modifier 'super)
+ '(mac-option-modifier 'meta)
+ '(mac-right-option-modifier nil)
+ '(emulate-mac-brittish-keyboard-mode t)
+ )
+
+(delete-selection-mode 1)
 
 (define-key smartparens-mode-map (kbd "M-<backspace>") 'sp-backward-kill-sexp)
-(define-key smartparens-mode-map (kbd "M-<delete>") 'sp-forward-kill-sexp)
+(define-key smartparens-mode-map (kbd "M-<delete>") 'sp-kill-sexp)
 
 (global-set-key (kbd "C-x t") 'quick-todo)
